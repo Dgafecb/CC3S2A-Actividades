8725872bcb4bea2d8205cdae8eb8410e58873248
Dgafecb <jdelrosarios@uni.pe>
2024-09-04 09:10:16 -0500

    add pipeline CI

--- Actividad1-Introduccion/devops-practice/src/app.js
+++ Actividad1-Introduccion/devops-practice/src/app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => {
-res.send('Hello, World!');
+    app.get('/', (req, res) => {
+    res.send('Hello, World!');
 });
 module.exports=app;
 if(require.main===module){
-const port = process.env.PORT || 0;
-app.listen(port, () => {
-console.log('Server running on port ${port}');
-});
+    const port = process.env.PORT || 0;
+    app.listen(port, () => {
+    console.log('Server running on port ${port}');
+    });
 }
 /*
 ----
